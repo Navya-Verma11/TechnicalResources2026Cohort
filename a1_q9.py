dd7e47e0498ae9b0d2b44547f802ad3d21cdc6a5
Navya-Verma11 <118188894+Navya-Verma11@users.noreply.github.com>
2023-01-19 11:24:39 +0530

    Submitting Q9 of Assignment 1

---
+++ a1_q9.py
@@ -0,0 +1,13 @@
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+def lcm(x, y):
+    return (x * y) // gcd(x, y)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("The GCD of", num1,"and", num2,"is", gcd(num1, num2))
+print("The LCM of", num1,"and", num2,"is", lcm(num1, num2))
